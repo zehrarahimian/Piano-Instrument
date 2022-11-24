8e1a16d3191ac659ff9c82d6bae58aa4e4bceabd
Zahra Rahimian <zehrarahimian@gmail.com>
2022-11-24 11:49:00 +0330

    Add sounds + Box collider + Audio surce + Start scripting

--- Assets/Scripts/Piano.cs
+++ Assets/Scripts/Piano.cs
@@ -15,40 +15,77 @@ public class Piano : MonoBehaviour
         switch(name)
         {
             case "E":
-            audioSource.PlayOneShot (audioClip[0]);
-            break;
-            case "D":
-            audioSource.PlayOneShot (audioClip[1]);
-            break;
-            case "C":
-            audioSource.PlayOneShot (audioClip[2]);
-            break;
-            case "B":
-            audioSource.PlayOneShot (audioClip[3]);
-            break;
-            case "A":
-            audioSource.PlayOneShot (audioClip[4]);
-            break;
-            case "G":
-            audioSource.PlayOneShot (audioClip[5]);
-            break;
-            case "F":
-            audioSource.PlayOneShot (audioClip[6]);
-            break;
-            case "D#":
             audioSource.PlayOneShot (audioClip[7]);
             break;
+            case "D":
+            audioSource.PlayOneShot (audioClip[6]);
+            break;
+            case "C":
+            audioSource.PlayOneShot (audioClip[4]);
+            break;
+            case "B":
+            audioSource.PlayOneShot (audioClip[2]);
+            break;
+            case "A":
+            audioSource.PlayOneShot (audioClip[1]);
+            break;
+            case "G":
+            audioSource.PlayOneShot (audioClip[11]);
+            break;
+            case "F":
+            audioSource.PlayOneShot (audioClip[9]);
+            break;
+            case "D#":
+            audioSource.PlayOneShot (audioClip[5]);
+            break;
             case "C#":
-            audioSource.PlayOneShot (audioClip[8]);
+            audioSource.PlayOneShot (audioClip[3]);
             break;
             case "A#":
-            audioSource.PlayOneShot (audioClip[9]);
+            audioSource.PlayOneShot (audioClip[0]);
             break;
             case "G#":
             audioSource.PlayOneShot (audioClip[10]);
             break;
             case "F#":
+            audioSource.PlayOneShot (audioClip[8]);
+            break;
+
+            case "E2":
+            audioSource.PlayOneShot (audioClip[7]);
+            break;
+            case "D2":
+            audioSource.PlayOneShot (audioClip[6]);
+            break;
+            case "C2":
+            audioSource.PlayOneShot (audioClip[4]);
+            break;
+            case "B2":
+            audioSource.PlayOneShot (audioClip[2]);
+            break;
+            case "A2":
+            audioSource.PlayOneShot (audioClip[1]);
+            break;
+            case "G2":
             audioSource.PlayOneShot (audioClip[11]);
+            break;
+            case "F2":
+            audioSource.PlayOneShot (audioClip[9]);
+            break;
+            case "D#2":
+            audioSource.PlayOneShot (audioClip[5]);
+            break;
+            case "C#2":
+            audioSource.PlayOneShot (audioClip[3]);
+            break;
+            case "A#2":
+            audioSource.PlayOneShot (audioClip[0]);
+            break;
+            case "G#2":
+            audioSource.PlayOneShot (audioClip[10]);
+            break;
+            case "F#2":
+            audioSource.PlayOneShot (audioClip[8]);
             break;
             
         }
